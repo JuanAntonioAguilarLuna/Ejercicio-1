52885af505d85598023ec625fb52b75ba98cfe1b
DESKTOP-I9U7JR1\Antonio <j.antonio.aguilar_luna@outlook.com>
2021-11-30 12:48:31 -0600

    operaciones

--- codigo4.js
+++ codigo4.js
@@ -1,15 +1,16 @@
+const dividir = (num1,num2)=>{
+    return parseInt(num1) / parseInt(num2);
+}
+const multiplicar = (num1,num2)=>{
+    return parseInt(num1) * parseInt(num2);
+}
 const sumar = (num1,num2)=>{
     return parseInt(num1) + parseInt(num2);
 }
 const restar = (num1,num2)=>{
-    return parseInt(num1) + parseInt(num2);
+    return parseInt(num1) - parseInt(num2);
 }
-const dividir = (num1,num2)=>{
-    return parseInt(num1) + parseInt(num2);
-}
-const multiplicar = (num1,num2)=>{
-    return parseInt(num1) + parseInt(num2);
-}
+
 
 alert("¿que operacion deseas realizar");
 operacion = prompt("1: suma, 2: resta, 3: division, 4: multiplicacion");
